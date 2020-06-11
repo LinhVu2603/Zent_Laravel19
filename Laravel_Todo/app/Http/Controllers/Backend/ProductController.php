673c8dd50100125b3113b8a337765fe07928f7f1
LinhVu2603 <Olivervubao@gmail.com>
2020-06-11 20:56:25 +0700

    Laravel session 7.3

--- Laravel_Todo/app/Http/Controllers/Backend/ProductController.php
+++ Laravel_Todo/app/Http/Controllers/Backend/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        // return view('backend.products.index');
+        return view('backend.products.index');
     }
 
     /**
@@ -24,7 +24,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        // return view('backend.products.create');
+        return view('backend.products.create');
     }
 
     /**
